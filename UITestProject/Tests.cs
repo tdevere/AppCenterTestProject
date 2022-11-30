39d246cc3084114891ad90de5cfa07190be274f3
tdevere <tdevere@microsoft.com>
2022-11-29 19:32:21 -0600

    first commit

---
+++ UITestProject/Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITestProject
+{
+
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+
+            
+        }
+
+        [Test]
+        public void UITestScreen()
+        {
+            //AppResult[] results = app.WaitForElement(c => c.Class
+            //AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
+            //app.Screenshot("Welcome screen.");
+            //Assert.IsTrue(results.Any());
+            //btnMyButton
+
+            app.Tap(c => c.Marked("btnMyButton"));
+            app.Screenshot("UI TEST Screen");
+
+        }
+
+        [Test]
+        public void UITestScreen2()
+        {
+            //AppResult[] results = app.WaitForElement(c => c.Class
+            //AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
+            //app.Screenshot("Welcome screen.");
+            //Assert.IsTrue(results.Any());
+            //btnMyButton
+
+            app.Tap(c => c.Marked("btnMyButton"));
+            app.Screenshot("UI TEST Screen 2");
+
+        }
+    }
+}
